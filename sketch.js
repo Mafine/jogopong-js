a4be82aad515a53326a1b31f5b1ed487029a784c
negao <141154764+Mafine@users.noreply.github.com>
2023-08-01 11:40:17 -0300

    Update sketch.js
    colando

--- sketch.js
+++ sketch.js
@@ -1,15 +1,89 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/p5.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/addons/p5.sound.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <meta charset="utf-8" />
+//variáveis da bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 15;
+let raio = diametro / 2 ;
 
-  </head>
-  <body>
-    <main>
-    </main>
-    <script sketch.jssrc="sketch.js"></script>
-  </body>
-</html>
+//velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
+  mostraRaquete();
+  mostraRaqueteOponente();
+  movimentaMinhaRaquete();
+  movimentaRaqueteOponenete();
+  verificaColisaoRaquete();
+}
+
+function mostraBolinha(){
+  circle(xBolinha, yBolinha, diametro);
+}
+
+function movimentaBolinha(){
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+}
+
+function verificaColisaoBorda(){
+  if (xBolinha + raio> width ||
+     xBolinha - raio< 0){
+    velocidadeXBolinha *= -1;
+  }
+  if (yBolinha + raio> height ||
+     yBolinha - raio < 0){
+    velocidadeYBolinha *= -1;
+  }
+}
+
+
+function mostraRaquete(){
+  rect(xRaquete, yRaquete, raqueteComprimento,
+      raqueteAltura);
+}
+
+function mostraRaqueteOponente(){
+rect (xRaqueteOponente, yRaqueteOponente, raqueteComprimento, raqueteAltura);
+
+}
+
+function movimentaMinhaRaquete(){
+  if (keyIsDown(UP_ARROW)){
+    yRaquete -= 10;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    yRaquete += 10;
+  }
+}
+
+function movimentaRaqueteOponenete () {
+ velocidadeYOponente = yBolinha - yRaqueteOponente - raqueteComprimento/2 - 30;
+  yRaqueteOponente += velocidadeYOponente;
+}
+  
+  
+function verificaColisaoRaquete(){
+  if (xBolinha - raio < xRaquete + raqueteComprimento &&
+      yBolinha - raio < yRaquete + raqueteAltura &&
+      yBolinha + raio > yRaquete){
+    velocidadeXBolinha *= -1;
+  }
+}
+  
+  
